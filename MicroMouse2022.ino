392a18cccd5e0e7f172885415844f5d26f641885
Dilraj Gill <=>
2022-10-29 00:09:19 -0700

    Added pin number variables

---
+++ MicroMouse2022.ino
@@ -0,0 +1,47 @@
+
+// Micromouse pinout
+// Left Motor
+const uint8_t IN1_pin = 7;  // blue
+const uint8_t IN2_pin = 6;  // green
+const uint8_t ENA_pin = 2;
+
+// Right Motor
+const uint8_t IN3_pin = 4;  // yellow
+const uint8_t IN4_pin = 5;  // red
+const uint8_t ENB_pin = 3;
+
+// Encoders
+const uint8_t left_encoder_A_pin = 21;    // blue IIII
+const uint8_t left_encoder_B_pin = 20;    // blue III
+const uint8_t right_encoder_A_pin = 23;   // blue II
+const uint8_t right_encoder_B_pin = 22;   // blue I
+
+// Ultrasonic Sensor
+const uint8_t ultrasonic_pin = 14;    // white
+
+// IR Sensors
+const uint8_t IR_left_pin = 16;   // white II
+const uint8_t IR_right_pin = 17;  // white I
+
+// Button
+const uint8_t button_pin = 30;  // gray/white
+
+// Dip Switches
+const uint8_t dip_1_pin = 35;   // red
+const uint8_t dip_2_pin = 36;   // red
+const uint8_t dip_3_pin = 37;   // red
+const uint8_t dip_4_pin = 38;   // red
+
+// RGB LED
+const uint8_t rgb_led_pin = 1;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

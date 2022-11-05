e73cd9aae2b8199349a4e444328a78964536eaa2
Patrick Leiser <patrick27leiser@yahoo.com>
2022-11-04 18:25:24 -0700

    basic usrf measurement and reporting on LED

--- MicroMouse2022.ino
+++ MicroMouse2022.ino
@@ -48,15 +48,54 @@ void setup() {
   pinMode(led_pin, OUTPUT);
   digitalWrite(IN3_pin, HIGH);
   digitalWrite(IN4_pin, LOW);
-  digitalWrite(ENB_pin, HIGH);
+  digitalWrite(ENB_pin, LOW);
 }
 
 void loop() {
-  digitalWrite(led_pin, LOW);
-  delay(1000);
-  digitalWrite(led_pin, HIGH);
-  delay(1000);
+  //digitalWrite(led_pin, LOW);
+  //delay(1000);
+  //digitalWrite(led_pin, HIGH);
+  //delay(1000);
   // put your main code here, to run repeatedly:
+  long distanceus = usrfIn();
+  long inch = microsecondsToInches(distanceus);
+  if (inch < 5){
+    digitalWrite(led_pin, HIGH);
+  } else {
+     digitalWrite(led_pin, LOW);
+  }
+  if (inch < 10){
+    digitalWrite(rgb_led_pin, HIGH);
+  } else {
+     digitalWrite(rgb_led_pin, LOW);
+  }
+}
 
 
+long usrfIn(){
+  pinMode(ultrasonic_pin,OUTPUT);
+  digitalWrite(ultrasonic_pin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(ultrasonic_pin,HIGH);
+  delayMicroseconds(5);
+  digitalWrite(ultrasonic_pin,LOW);
+  pinMode(ultrasonic_pin,INPUT);
+  return (pulseIn(ultrasonic_pin, HIGH));
+  
+}
+
+long microsecondsToInches(long microseconds) {
+  // According to Parallax's datasheet for the PING))), there are 73.746
+  // microseconds per inch (i.e. sound travels at 1130 feet per second).
+  // This gives the distance travelled by the ping, outbound and return,
+  // so we divide by 2 to get the distance of the obstacle.
+  // See: https://www.parallax.com/package/ping-ultrasonic-distance-sensor-downloads/
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
 }
